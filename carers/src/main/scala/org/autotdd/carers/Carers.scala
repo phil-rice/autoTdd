9cbbe6f3bef305188d7c34ea5c7a9933496a9aed
Phil <phil.rice.erudine@googlemail.com>
2013-09-25 11:03:15 +0100

    More carers testcases

--- carers/src/main/scala/org/autotdd/carers/Carers.scala
+++ carers/src/main/scala/org/autotdd/carers/Carers.scala
@@ -46,6 +46,16 @@ object Carers {
     expected(ReasonAndAmount("carer.claimant.under35hoursCaring")).
     because((w: World, e: Elem) => !Xmls.asYesNo((e \\ "ClaimData" \ "Claim35Hours").text)).
 
+    useCase("Qualifying Benefit 3 - DP's without the required level of qualyfing benefit will result in the disallowance of the claim to CA.").
+    scenario(World("2010-6-23"), Xmls.validateClaim("CL100106A"), "CL100106A-?????? ").
+    expected(ReasonAndAmount("carer.qualifyingBenefit.dpWithoutRequiredLevelOfQualifyingBenefit")).
+      because((w: World, e: Elem) => {
+      val dependantNino = (e \\ "DependantNINO").text;
+      val dependantXml = w.ninoToCis(dependantNino);
+      (dependantXml \\ "AwardComponent").text != "DLA Middle Rate Care"
+    }).
+
+    
     useCase("Residence 3- Customer who is not considered resident and present in GB is not entitled to CA.").
     scenario(World("2010-6-7"), Xmls.validateClaim("CL100107A"), "CL100107A-notInGB").
     expected(ReasonAndAmount("carers.claimant.notResident")).
@@ -86,14 +96,6 @@ object Carers {
     scenario(World("2010-3-8"), Xmls.validateClaim("CL100111A"), "CL100111A-private pension").
     expected(ReasonAndAmount("carers.validClaim", Some(95))).
 
-    useCase("Sublet 2- Customers receiving payment for subletting their property for board and lodgings receiving more than the prescribed limit of �100 (after allowable expenses) will be disallowed for CA.").
-    scenario(World("2010-3-1"), Xmls.validateClaim("CL100115A"), "CL115A-sub let").
-    expected(ReasonAndAmount("carers.income.rental", None)).
-    because((w: World, e: Elem) => Xmls.asYesNo((e \\ "ClaimRentalIncome").text)).
-    //    because((w: World, e: Elem) => {
-    //      Xmls.asYesNo((e \\ "ClaimData" \ "ClaimRentalIncome").text)
-    //    }).
-
     useCase("Employment 6 - Customers claiming CA may claim an allowable expense of up to 50% of their Occupational Pension contributions. This amount may then be deducted from their gross pay figure.").
     scenario(World("2010-3-8"), Xmls.validateClaim("CL100112A"), "CL100112A-occupational pension").
     expected(ReasonAndAmount("carers.validClaim", Some(95))).
@@ -101,20 +103,32 @@ object Carers {
     //    scenario(World.blankTestWorld, Xmls.validateClaim("CL100106A"), "CL100106A-No Qualifying Benefit").
     //    expected(ReasonAndPayment("carer.dp.withoutLevelOfQualifyingBenefit")).
     //    because((w: World, e: Elem) => true).
+    
 
     useCase("Employment 7 - Customer in paid employment exceeding �100 (after allowable expenses) per week is not entitled to CA.").
     scenario(World("2010-6-1"), Xmls.validateClaim("CL100113A"), "CL100113A-paid employment earning too much").
     expected(ReasonAndAmount("carers.nettIncome.moreThan100PerWeek", None)).
     because((w: World, e: Elem) => {
-      val expenses: Double = calculateExpenses(w, e).amount.getOrElse(0);
-      val weeklyIncome: Double = calculateWeeklyIncome(w, e)
-      val nett = weeklyIncome - expenses
-      nett > 100
+    	val expenses: Double = calculateExpenses(w, e).amount.getOrElse(0);
+    val weeklyIncome: Double = calculateWeeklyIncome(w, e)
+    		val nett = weeklyIncome - expenses
+    		nett > 100
     }).
+
 
     useCase("Self employment 2 - Customer in Self employed work earning more than the prescribed limit of �100 per week (after allowable expenses) are not entitled to CA.").
     scenario(World("2010-3-1"), Xmls.validateClaim("CL100114A"), "CL114A-self employed earning too much").
     expected(ReasonAndAmount("carers.nettIncome.moreThan100PerWeek", None)).
+
+    useCase("Sublet 2- Customers receiving payment for subletting their property for board and lodgings receiving more than the prescribed limit of �100 (after allowable expenses) will be disallowed for CA.").
+    scenario(World("2010-3-1"), Xmls.validateClaim("CL100115A"), "CL115A-sub let").
+    expected(ReasonAndAmount("carers.income.rental", None)).
+    because((w: World, e: Elem) => Xmls.asYesNo((e \\ "ClaimRentalIncome").text)).
+    
+    useCase("Prop 2- Customer receiving an Income from the renting of another property or land in the UK or abroad either their own name or a share in a partners profit is above £100 per week(after allowable expenses) is not entitled to CA.").
+    scenario(World("2010-3-1"), Xmls.validateClaim("CL100116A"), "CL116A-income from renting").
+    expected(ReasonAndAmount("carers.income.rental", None)).
+    
 
     useCase("Overlapping benefit 2 - Customer receiving certain other benefits at a rate lower than the rate of CA will reduce the payable amount of CA.").
     scenario(World("2010-5-5"), Xmls.validateClaim("CL100117A"), "CL100117A-self employed earning too much").
@@ -124,7 +138,10 @@ object Carers {
       val dependantXml = w.ninoToCis(dependantNino);
       (dependantXml \\ "AwardComponent").text == "DLA Middle Rate Care"
     }).
-
+    
+    useCase("Break 6 - Break in care, at a hospital for 2 weeks - still gets paid").
+    scenario(World("2010-5-5"), Xmls.validateClaim("CL100119A"), "CL100119A-dp in hospital for 2 weeks").
+    expected(ReasonAndAmount("carers.validClaim", Some(95))).
     build;
 
   def main(args: Array[String]) {
